5d528172ab7abf0831ab9509676f74e84a51b2c2
lmacchiavelli <lmacchiavelli@gmail.com>
2019-03-15 15:06:30 +0100

    Update pannello-editing-style.js

--- docs/pannello-editing-style.js
+++ docs/pannello-editing-style.js
@@ -1,3 +1,25 @@
+"use strict";
+
+var base_style="\
+.stage{\
+    display:flex;\
+    flex-flow:row wrap;\
+}\
+.text-code{\
+    height:100%;\
+    width:100%;\
+    position:absolute;\
+    left:0px;\
+    top:0px;\
+}\
+";
+
+var base_interface = "\
+<div class='stage'>\
+<div><textarea></textarea></div></div></div>\
+</div>\
+";
+
 
 $(document).ready(function(){
     $('body').prepend('<div>Pannello</div>')
